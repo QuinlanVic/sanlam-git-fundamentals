e300b75ff9c8968444d1045331a2b5bc78388ac5
QuinlanVic <Quinlanvc@gmail.com>
2024-03-06 13:28:42 +0200

    Added getprofilepic to fun

--- fun.py
+++ fun.py
@@ -1,5 +1,11 @@
+import profile_pic
+
+
 def sum2(a, b, c):
     return a + b + c
 
 
-print(sum(4, 5, 6))
+if __name__ == "__main__":
+    print(sum(4, 5, 6))
+    print("Updating my master with fun funcitonality✌️✌️👍")
+    print(profile_pic.get_profile_pic())
